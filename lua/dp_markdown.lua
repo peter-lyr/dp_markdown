891278d3b61ee95473058b634a0fe7ac85d9e596
peter-lyr <llydrp.ldp@gmail.com>
2024-04-13 20:24:46 +0800

    B.copy2clip_exe

--- lua/dp_markdown.lua
+++ lua/dp_markdown.lua
@@ -61,7 +61,7 @@ function M.buffer_open_cfile()
     M.file_stack[#M.file_stack + 1] = cfile
   else
     if B.is(cfile) and B.is_dir(cfile) then
-      require 'config.nvim.nvimtree'.open(cfile)
+      B.cmd('e %s', cfile)
     else
       B.echo('not a file: %s', cfile)
     end
@@ -135,11 +135,10 @@ function M.copy_cfile_clip()
       local ext = string.match(cfile, '%.([^.]+)$')
       local rename_file = name .. '.' .. ext
       local newfile = B.get_filepath(B.windows_temp, rename_file)
-      local copy2clip_exe = require 'config.nvim.nvimtree'.copy2clip_exe
-      B.system_run('start silent', 'copy /y "%s" "%s" && %s "%s"', B.rep(cfile), newfile, copy2clip_exe, newfile)
+      B.system_run('start silent', 'copy /y "%s" "%s" && %s "%s"', B.rep(cfile), newfile, B.copy2clip_exe, newfile)
       B.notify_info(newfile .. ' copied')
     else
-      B.system_run('start silent', '%s "%s"', copy2clip_exe, cfile)
+      B.system_run('start silent', '%s "%s"', B.copy2clip_exe, cfile)
       B.notify_info(cfile .. ' copied')
     end
   else
