3cdccc93489c865970d813d019361f4d233fc996
peter-lyr <llydrp.ldp@gmail.com>
2024-04-30 00:14:14 +0800

    function M.paste_image(image_type)

--- lua/dp_markdown/image/paste.lua
+++ lua/dp_markdown/image/paste.lua
@@ -10,7 +10,7 @@ M.lua = B.getlua(M.source)
 
 M.image_root_dir_name = '.images'
 M.image_root_dir_md_name = '_.md'
--- M.image_paste_temp_name = 'nvim_paste_temp'
+M.image_paste_temp_name = 'nvim_paste_temp'
 
 M.last_file = ''
 M.last_lnr = 1
@@ -31,7 +31,7 @@ function M.is_in_image_fts(file)
   return B.is_file_in_extensions(file, M.IMAGE_EXTS)
 end
 
-function M.paste_image(image_file, markdown_file, lnr)
+function M.drag_image(image_file, markdown_file, lnr)
   local proj_root = vim.fn['ProjectRootGet'](markdown_file)
   if not B.is(proj_root) then
     B.notify_info('not in a project root: ' .. markdown_file)
@@ -43,9 +43,9 @@ function M.paste_image(image_file, markdown_file, lnr)
   local image_hash_8 = string.sub(image_hash_64, 1, 8)
   local image_fname_tail = vim.fn.fnamemodify(image_file, ':t')
   local image_fname_tail_root = vim.fn.fnamemodify(image_fname_tail, ':r')
-  -- if image_fname_tail_root == M.image_paste_temp_name then
-  --   image_fname_tail_root = vim.fn.strftime '%Y%m%d-%A-%H%M%S'
-  -- end
+  if image_fname_tail_root == M.image_paste_temp_name then
+    image_fname_tail_root = vim.fn.strftime '%Y%m%d-%H%M%S'
+  end
   local image_fname_tail_ext = vim.fn.fnamemodify(image_fname_tail, ':e')
   local image_hash_name = image_hash_8 .. '.' .. image_fname_tail_ext
   local image_target_file = B.getcreate_filepath(image_root_dir, image_hash_name).filename
@@ -94,11 +94,115 @@ B.aucmd('BufReadPost', 'markdown.image.paste.BufReadPost', {
     M._cur_file = B.buf_get_name(ev.buf)
     if M.is_in_markdown_fts(M.last_file) and M._dragging then
       if M.is_in_image_fts(M._cur_file) then
-        M.paste_image(M._cur_file, M.last_file, M.last_lnr)
+        M.drag_image(M._cur_file, M.last_file, M.last_lnr)
         return
       end
     end
   end,
 })
+
+function M.paste_image(image_type)
+  if not vim.g.paste_image_allowed and not M.is_to_paste_image() then
+    return
+  end
+  vim.g.temp_image_file_with_ext = B.rep(B.getcreate_temp_dirpath 'nvim_paste_image':joinpath(M.image_paste_temp_name).filename)
+  vim.g.temp_image_file = image_type and vim.g.temp_image_file_with_ext .. '.' .. image_type or ''
+  vim.g.temp_image_type = image_type and image_type or ''
+  vim.g.paste_image_allowed = nil
+  vim.cmd [[
+    python << EOF
+import vim
+import subprocess
+temp_image_file = vim.eval('g:temp_image_file')
+temp_image_type = vim.eval('g:temp_image_type')
+temp_image_file_with_ext = vim.eval('g:temp_image_file_with_ext')
+script_code = f"""
+Add-Type -AssemblyName System.Windows.Forms;
+if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {{
+  $imageObj = [System.Drawing.Bitmap][System.Windows.Forms.Clipboard]::GetDataObject().getimage();
+}}
+"""
+if temp_image_type:
+  script_code += f'''
+  $imageObj.Save("{temp_image_file}", [System.Drawing.Imaging.ImageFormat]::{temp_image_type});
+'''
+else:
+  script_code += f'''
+  $imageObj.Save("{temp_image_file_with_ext}.jpg", [System.Drawing.Imaging.ImageFormat]::Jpeg);
+  $imageObj.Save("{temp_image_file_with_ext}.png", [System.Drawing.Imaging.ImageFormat]::Png);
+'''
+completed_process = subprocess.run(["powershell.exe", "-Command", script_code], capture_output=True, text=True)
+if completed_process.returncode:
+  print(f"Error: {completed_process.stderr}")
+else:
+  vim.command('let g:paste_image_allowed = 1')
+EOF
+]]
+  if vim.g.paste_image_allowed then
+    if not image_type then
+      image_type = vim.fn.getfsize(vim.g.temp_image_file_with_ext .. '.jpg') > vim.fn.getfsize(vim.g.temp_image_file_with_ext .. '.png') and 'png' or 'jpg'
+    end
+    B.set_timeout(10, function()
+      M.drag_image(vim.g.temp_image_file_with_ext .. '.' .. image_type, B.buf_get_name(), vim.fn.line '.')
+    end)
+  end
+  vim.g.paste_image_allowed = nil
+end
+
+function M.is_to_paste_image()
+  vim.g.paste_image_allowed = nil
+  vim.cmd [[
+    python << EOF
+import vim
+try:
+  from PIL import ImageGrab, Image
+except:
+  import os
+  cmd = "pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host mirrors.aliyun.com"
+  print(cmd)
+  os.system(cmd)
+  from PIL import ImageGrab, Image
+img = ImageGrab.grabclipboard()
+if isinstance(img, Image.Image):
+  vim.command('let g:paste_image_allowed = 1')
+EOF
+  ]]
+  local file = B.buf_get_name()
+  if not M.is_in_markdown_fts(file) then
+    B.print('not a markdown file: ' .. file)
+    return nil
+  end
+  local project = B.get_proj_root(file)
+  if #project == 0 then
+    B.print('not in a project root: ' .. file)
+    return nil
+  end
+  return vim.g.paste_image_allowed
+end
+
+function M.middlemouse()
+  if vim.fn.getmousepos()['line'] == 0 then
+    return '<MiddleMouse>'
+  end
+  if M.is_to_paste_image() then
+    M.paste_image()
+  end
+  return '<MiddleMouse>'
+end
+
+function M.s_middlemouse()
+  if vim.fn.getmousepos()['line'] == 0 then
+    return '<S-MiddleMouse>'
+  end
+  if M.is_to_paste_image() then
+    M.paste_image 'png'
+  end
+  return '<S-MiddleMouse>'
+end
+
+require 'which-key'.register {
+  ['<MiddleMouse>'] = { function() M.middlemouse() end, 'image.paste: small', expr = true, mode = { 'n', 'v', }, silent = true, },
+  ['<S-MiddleMouse>'] = { function() M.s_middlemouse() end, 'image.paste: png', expr = true, mode = { 'n', 'v', }, silent = true, },
+}
 
 return M
