c98f574d6face5dff36be94900ce163d189f7ba1
peter-lyr <llydrp.ldp@gmail.com>
2024-06-10 00:24:15 +0800

    format code

--- lua/dp_markdown/.url.lua/update_url.py
+++ lua/dp_markdown/.url.lua/update_url.py
@@ -38,40 +38,40 @@ if __name__ == "__main__":
             for line in lines:
                 new_line = line
                 res = re.findall(patt, line)
-                if res:
-                    for r in res:
-                        d = os.path.join(dirname, r[1])
-                        if not os.path.exists(d):
-                            continue
-                        rname = r[2]
-                        f = os.path.join(d, rname)
-                        if not os.path.exists(f):
-                            if d not in Files:
-                                Files[d] = []
-                                FilesShort[d] = []
-                                for _d, _, _fs in os.walk(d):
-                                    for _f in _fs:
-                                        # if _f.split(b".")[-1].lower() != b"md":
-                                        #     continue
-                                        Files[d].append(os.path.join(_d, _f).lower())
-                                        FilesShort[d].append(_f.split(b"/")[-1].lower())
-                            fname = rname.split(b"/")[-1].lower()
-                            c =  FilesShort[d].count(fname)
-                            if printed == 0:
-                                printed = 1
-                                print(file)
-                            print(c, '  ', rname.decode('utf-8'))
-                            if (
-                                FilesShort[d].count(fname) == 1
-                            ):  # 只处理只找到一个相同文件名的情况,不存在或存在多个相同文件名的不处理
-                                new_rname = os.path.relpath(Files[d][FilesShort[d].index(fname)], d).replace(b"\\", b"/")
-                                new_line = new_line.replace( rname, new_rname)
-                                allowed = 1
-                                _t = ''
-                                for _ in range(len(str(c))):
-                                    _t += ' '
-                                print(_t, '->', new_rname.decode('utf-8'))
-                new_lines.append(new_line)
+                if not res:
+                    new_lines.append(new_line)
+                    continue
+                for r in res:
+                    d = os.path.join(dirname, r[1])
+                    if not os.path.exists(d):
+                        continue
+                    rname = r[2]
+                    f = os.path.join(d, rname)
+                    if os.path.exists(f):
+                        continue
+                    if d not in Files:
+                        Files[d] = []
+                        FilesShort[d] = []
+                        for _d, _, _fs in os.walk(d):
+                            for _f in _fs:
+                                Files[d].append(os.path.join(_d, _f).lower())
+                                FilesShort[d].append(_f.split(b"/")[-1].lower())
+                    fname = rname.split(b"/")[-1].lower()
+                    c =  FilesShort[d].count(fname)
+                    if printed == 0:
+                        printed = 1
+                        print(file)
+                    print(c, '  ', rname.decode('utf-8'))
+                    if (
+                        FilesShort[d].count(fname) == 1
+                    ):  # 只处理只找到一个相同文件名的情况,不存在或存在多个相同文件名的不处理
+                        new_rname = os.path.relpath(Files[d][FilesShort[d].index(fname)], d).replace(b"\\", b"/")
+                        new_line = new_line.replace( rname, new_rname)
+                        allowed = 1
+                        _t = ''
+                        for _ in range(len(str(c))):
+                            _t += ' '
+                        print(_t, '->', new_rname.decode('utf-8'))
             if allowed:
                 with open(file, "wb") as f:
                     f.writelines(new_lines)
